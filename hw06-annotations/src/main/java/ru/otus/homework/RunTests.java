6bd51f4030159337356edc6dbcadbbf59e05c3cd
Stas <zstanislaw211@gmail.com>
2022-08-21 18:51:31 +0300

    Done.

--- hw06-annotations/src/main/java/ru/otus/homework/RunTests.java
+++ hw06-annotations/src/main/java/ru/otus/homework/RunTests.java
@@ -1,8 +1,7 @@
 package ru.otus.homework;
 
-import ru.otus.reflection.SimpleAnnotation;
-
 import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,19 +10,21 @@ public class RunTests {
     public RunTests() {
     }
 
-    public void runTests(String className) throws ClassNotFoundException {
+    public void runTests(String className) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        int successfulTests = 0;
+        int failedTests = 0;
         Class<?> clazz = Class.forName(className);
         System.out.println("Testing class '" + clazz.getSimpleName() + "'");
 
         Method[] methods = clazz.getMethods();
         //достаем методы с аннотацией @Test
-        List<Method> testMethods = getMethodsWithСertainAnnotation(methods, Test.class);
+        List<Method> testMethods = getMethodsWithAnnotation(methods, Test.class);
 
         //достаем методы с аннотацией @Before
-        List<Method> beforeMethods = getMethodsWithСertainAnnotation(methods, Before.class);
+        List<Method> beforeMethods = getMethodsWithAnnotation(methods, Before.class);
 
         //достаем методы с аннотацией @After
-        List<Method> afterMethods = getMethodsWithСertainAnnotation(methods, After.class);
+        List<Method> afterMethods = getMethodsWithAnnotation(methods, After.class);
 
 
         //создаем последовательности тестирования - каждому методу @Test предшествуют все доступные в классе @Before и @After
@@ -31,26 +32,48 @@ public class RunTests {
 
         for (Method testMethod : testMethods) {
             //Создаем под каждый @Test отдельный инстанс
+            var constructor = clazz.getConstructor();
+            var object = constructor.newInstance();
+
 
             //выполняем все @Before
-            for(Method beforeMethod: beforeMethods){
-
+            for (Method beforeMethod : beforeMethods) {
+                try {
+                    beforeMethod.invoke(object);
+                    successfulTests++;
+                } catch (Exception ex) {
+                    failedTests++;
+                }
             }
             //выполняем собственно @Test
-
+            try {
+                testMethod.invoke(object);
+                successfulTests++;
+            } catch (Exception ex) {
+                failedTests++;
+            }
 
             //выполняем все @After
-            for(Method afterMethod: afterMethods){
-
+            for (Method afterMethod : afterMethods) {
+                try {
+                    afterMethod.invoke(object);
+                    successfulTests++;
+                } catch (Exception ex) {
+                    failedTests++;
+                }
             }
         }
+        System.out.println("Whole count of tests = " + (successfulTests + failedTests));
+        System.out.println(
+                "Count of successfully passed tests = " + successfulTests);
+        System.out.println("Count of failed tests = " + failedTests);
     }
 
-    private ArrayList<Method> getMethodsWithСertainAnnotation(Method[] methods, Class<? extends Annotation> annotationClazz) throws ClassNotFoundException {
+    private ArrayList<Method> getMethodsWithAnnotation(Method[] methods, Class<? extends Annotation> annotationClazz) throws ClassNotFoundException {
         ArrayList<Method> listMethods = new ArrayList<>();
-        System.out.println("-----------достаем методы с аннотацией @Test");
+        //System.out.println("-----------достаем методы с аннотацией @Test");
         for (Method method : methods) {
-            System.out.println("-----------------------------------Метод:" + method.toString());
+            //System.out.println("-----------------------------------Метод:" + method.toString());
             if (method.isAnnotationPresent((Class<? extends Annotation>) Class.forName(annotationClazz.getCanonicalName()))) {
                 listMethods.add(method);
             }
@@ -58,7 +81,7 @@ public class RunTests {
         return listMethods;
     }
 
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         RunTests runTests = new RunTests();
         runTests.runTests(Testable.class.getCanonicalName());
     }
--- hw06-annotations/src/main/java/ru/otus/homework/Testable.java
+++ hw06-annotations/src/main/java/ru/otus/homework/Testable.java
@@ -3,8 +3,14 @@ package ru.otus.homework;
 public class Testable {
     //happy path, without exception, all works fine
     @Before
-    public void before() {
+    public void before1() {
         System.out.println("Before test");
+        throw new RuntimeException("Before test exception");
+    }
+
+    @Before
+    public void before2() {
+        System.out.println("Before test2");
     }
 
     @Test
@@ -16,6 +22,7 @@ public class Testable {
     public void after() {
         System.out.println("After test");
     }
+
 
     //happy path, without exception, all works fine
 
