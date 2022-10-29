092914822fd4d819e207107030e77d5309063e93
svladimirov <Dream_all_day93>
2022-10-29 19:26:35 +0300

    last issue - N+1 update

--- hw21/src/main/resources/migration/V1__initial_schema.sql
+++ hw21/src/main/resources/migration/V1__initial_schema.sql
@@ -16,3 +16,13 @@ create table client
     id   bigint not null primary key,
     name varchar(50)
 );
+create table address
+(
+    id   bigint not null primary key,
+    street varchar(50)
+);
+create table phone
+(
+    id   bigint not null primary key,
+    number varchar(50)
+);
