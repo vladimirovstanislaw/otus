c6c1cda116e03c21e85c368f5b90bc035d4d09f5
svladimirov <Dream_all_day93>
2022-12-08 18:35:26 +0300

    Done.

--- hw29/src/main/java/ru/otus/service/ClientServiceImpl.java
+++ hw29/src/main/java/ru/otus/service/ClientServiceImpl.java
@@ -2,6 +2,7 @@ package ru.otus.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.otus.repository.ClientRepository;
 import ru.otus.model.Client;
@@ -14,6 +15,7 @@ public class ClientServiceImpl implements ClientService {
     private static final Logger log = LoggerFactory.getLogger(ClientServiceImpl.class);
     private final ClientRepository clientRepository;
 
+    @Autowired
     public ClientServiceImpl(ClientRepository clientRepository) {
         this.clientRepository = clientRepository;
     }
